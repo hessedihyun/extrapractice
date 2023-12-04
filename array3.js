1dd404c26e9fd96fde3cd765eb317179f31e7b2b
hessedihyun <88070549+hessedihyun@users.noreply.github.com>
2023-12-05 00:12:06 +0900

    mission successed

--- array3.js
+++ array3.js
@@ -10,13 +10,15 @@ for(let i=0; i<arr.length; i++){
 }
 
 let [ar1, ar2, ar3, ar4, ar5, ar6, ar7, ar8, ar9, ar10] = arr;
-let k=``;
+// ar1은 arr[0]
 
-for(let j=0; j<arr.length; j++){
-
-    for(let i=0; i<arr.length; i++){
-        k += `${ar1[i]} `;
-    }
-    console.log(`ar${j} 배열 : ${k}`);
+for(let i=0; i<arr.length; i++){
+    let k =``;
+    
+    for(let j=0; j<arr.length; j++){
+            k += `${arr[i][j]} `;
+        }
+    
+    console.log(`ar${i+1}의 배열 : ${k}\n`);
 }
 
